f4ac2f70283ead4849ba98f18c5f71f8291318ae
creativeeyestudio <contact@creative-eye.fr>
2025-07-05 14:22:14 +0000

    Users label

--- src/collections/Users.ts
+++ src/collections/Users.ts
@@ -3,6 +3,10 @@ import type { CollectionConfig } from 'payload'
 
 export const Users: CollectionConfig = {
   slug: 'users',
+  labels: {
+    singular: 'Utilisateur',
+    plural: 'Utilisateurs',
+  },
   admin: {
     useAsTitle: 'email',
     group: 'Administration',
