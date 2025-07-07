95750dfb71ab63d0b842e78990a15fecf97727fc
creativeeyestudio <contact@creative-eye.fr>
2025-07-07 16:22:30 +0000

    Update PageAccess

--- src/access/pagesAccess.ts
+++ src/access/pagesAccess.ts
@@ -1,8 +1,7 @@
-import { Access } from 'payload/types'
 import { hasRole } from './roles'
 
 export const pagesAccess = {
-  read: ({ req: { user } }) => hasRole(user?.role, 'editor'),
+  read: () => true,
   create: ({ req: { user } }) => hasRole(user?.role, 'editor'),
   update: ({ req: { user } }) => hasRole(user?.role, 'editor'),
   delete: ({ req: { user } }) => hasRole(user?.role, 'editor'),
